ca0bdd65541a7ad1c32dc452685b72233d198643
Camron Naderi <camronnaderi57@gmail.com>
2023-05-21 17:08:13 -0700

    added setup input

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const { connect } = require('./client');
 
 connect()
+const handleUserInput = function (key) {
+console.log(key)
+if (key === '\u0003') {
+  process.exit()
+}
+process.stdout.write(key)
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput()
